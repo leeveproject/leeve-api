497806045fbe210b7ca7af2bf2b4863082766a48
23eerick <erickwelberth1@hotmail.com>
2023-09-24 12:28:05 -0300

    feat: create login user controllers

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -30,6 +30,43 @@ export async function addUsers(req, res) {
 
 }
 
+export async function loginUsers(req, res) {
+  const { FirstName, LastName, Password } = req.body
+
+  try {
+    const user = await Users.findOne({
+      where: {
+        FirstName,
+        LastName
+      }
+    })
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Unable to find user"
+      });
+    }
+
+    const isPasswordValid = await bcript.compare(Password, user.Password)
+
+    if (isPasswordValid) {
+      return res.status(200).json({
+        message: "You have successfully logged in"
+      });
+    }
+
+    return res.status(400).json({
+      message: "Incorrect password"
+    });
+
+
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error when logging in user"
+    });
+  }
+}
+
 export async function updateUsers(req, res) {
   try {
     const user = await Users.findByPk(req.params.id)
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addUsers, deleteUsers, getUsers, updateUsers } from "../controllers/users/index.js";
+import { addUsers, deleteUsers, getUsers, loginUsers, updateUsers } from "../controllers/users/index.js";
 
 export const Users = Router();
 
 Users.get("/users", getUsers);
 Users.post("/users", addUsers);
+Users.post("/login", loginUsers);
 Users.put("/users/:id", updateUsers);
 Users.delete("/users/:id", deleteUsers);
