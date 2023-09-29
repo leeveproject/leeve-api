b015f02f0f44643eac5c33733152db481df099d2
23eerick <erickwelberth1@hotmail.com>
2023-09-29 09:43:17 -0300

    test: initial tests for creating users

--- src/tests/product.test.js
+++ src/tests/product.test.js
@@ -1,5 +1,5 @@
 import request from "supertest";
-import { app } from "../../../app.js";
+import { app } from "../../app.js";
 
 describe("testing product controller in routes", () => {
 
---
+++ src/tests/user.test.js
@@ -0,0 +1,53 @@
+import request from "supertest";
+import { app } from "../../app.js";
+
+describe("testing users controller in routes", () => {
+
+  it("show all users", async () => {
+    const res = await request(app).get("/users")
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeInstanceOf(Array)
+  })
+
+  it("create new user", async () => {
+    const user = {
+      FirstName: "",
+      LastName: "",
+      Email: "",
+      Password: "",
+      Company: "",
+      Size: ""
+    }
+
+    const res = await request(app).post("/users")
+      .send(user)
+
+    expect(res.status).toBe(201)
+    expect(res.body).toBeInstanceOf(Object)
+  })
+
+  it("update existing user", async () => {
+    const user = {
+      FirstName: "",
+      LastName: "",
+      Email: "",
+      Password: "",
+      Company: "",
+      Size: ""
+    }
+
+    const res = await request(app).put("/users/:id")
+      .send(user)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeInstanceOf(Object)
+  })
+
+  it("delete existing user", async () => {
+    const res = await request(app).delete("/users/:id")
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeInstanceOf(Array)
+  })
+})
