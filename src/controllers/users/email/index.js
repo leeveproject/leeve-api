9343206a45aa7a5ad9170441ba4956ce18646a01
23eerick <erickwelberth1@hotmail.com>
2023-11-10 23:27:47 -0300

    Functional testing commands

--- src/controllers/users/email/index.js
+++
@@ -1,3 +0,0 @@
-export async function sendEmail(req, res) {
-  await res.status(200).json("test email")
-}
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { sendEmail } from "../controllers/users/email/index.js"
 import { addUsers, deleteUsers, getUsers, loginUsers, updateUsers } from "../controllers/users/index.js";
 
 export const Users = Router();
@@ -7,6 +6,5 @@ export const Users = Router();
 Users.get("/users", getUsers);
 Users.post("/users", addUsers);
 Users.post("/login", loginUsers);
-Users.get("/email", sendEmail);
 Users.put("/users/:id", updateUsers);
 Users.delete("/users/:id", deleteUsers);
